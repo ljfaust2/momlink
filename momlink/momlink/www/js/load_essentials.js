ee54d5c1ccad8cc4cdf33ae1771cdbd4894cc246
ljfaust2 <lfaust@nd.edu>
2016-04-05 14:48:58 -0400

    cleaned up strings

--- momlink/momlink/www/js/load_essentials.js
+++ momlink/momlink/www/js/load_essentials.js
@@ -1,23 +1,22 @@
-﻿document.write(
-'<!DOCTYPE html>'+
-'<html>'+
-    '<head>'+
-    '<meta charset="utf-8">'+
-    '<meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no, width=device-width">'+
-    '<title></title>'+
+document.write(
+`<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no, width=device-width">
 
-    '<link href="http://code.ionicframework.com/1.2.4/css/ionic.css" rel="stylesheet">'+
-    '<link href="../css/style.css" rel="stylesheet">'+
+    <link href="http://code.ionicframework.com/1.2.4/css/ionic.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
 
-    '<script src="http://code.ionicframework.com/1.2.4/js/ionic.bundle.min.js"></script>' +
+    <script src="http://code.ionicframework.com/1.2.4/js/ionic.bundle.min.js"></script>
 
-    '<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>'+
+    <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
 
-    '<!-- cordova script (this will be a 404 during development) -->'+
-    '<script src="cordova.js"></script>'+
+    <!-- cordova script (this will be a 404 during development) -->
+    <script src="cordova.js"></script>
 
-    "<!-- your app's js -->"+
-    '<script src="../js/app.js"></script>'+
-    '<script src="../js/controllers.js"></script>' +
-    '</head>'
+    <!-- your app's js -->
+    <script src="../js/app.js"></script>
+    <script src="../js/controllers.js"></script>
+    </head>`
 );
